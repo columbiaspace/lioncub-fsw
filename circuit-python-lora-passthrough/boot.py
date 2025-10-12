4f16c500df62cbf1acb817788ca4bc4c2776166b
Nate Gay <email@n8.gay>
2025-10-12 14:50:56 -0500

    Update boot.py (#57)

--- circuit-python-lora-passthrough/boot.py
+++ circuit-python-lora-passthrough/boot.py
@@ -1,7 +1,10 @@
 """
 boot.py
 
-TODO(nateinaction): I don't know what this file is for yet.
+This file helps to use an RP2350 CircuitPython environment as a passthrough for
+interacting with the radio and F Prime GDS. The usb_cdc.enable() function creates a
+USB serial endpoint that the GDS can connect to to receive all the data that is
+streamed out. The function has to be called in boot.py before the USB device is enabled.
 """
 
 import usb_cdc
